9f8bd80f8ad42f3cc79e7dfc4559d7d15297aa85
nithyakalathil <nithya913k@gmail.com>
2024-06-07 20:38:41 +0530

    album app created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Addalbum from './components/Addalbum';
+import Delete from './components/Delete';
+import Search from './components/Search';
+import Viewall from './components/Viewall';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Addalbum/>}/>
+      <Route path='/search' element={<Search/>}/>
+      <Route path='/delete' element={<Delete/>}/>
+      <Route path='/view' element={<Viewall/>}/>
+
+    </Routes>
+    </BrowserRouter>
+
   );
 }
 
